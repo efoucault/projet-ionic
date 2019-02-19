d91547d26bf505ca4f320133fdbda304b902467d
Eric Foucault <efoucault@gmail.com>
2019-02-19 22:47:41 +0100

    initialisation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppareilsPage } from '././appareils/appareils.page';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'appareils', component: AppareilsPage },
 ];
 
 @NgModule({
---
+++ src/app/appareils/appareils.page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page-appareils',
+  templateUrl: 'appareils.page.html'
+})
+export class AppareilsPage {
+  
+}
